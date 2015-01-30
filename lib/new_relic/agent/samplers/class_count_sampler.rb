66fb433b9f389d9cf679049b6bbe6604e8367f57
Ben Weintraub <ben@newrelic.com>
2015-01-29 18:06:11 -0800

    Get rid of class-counting sampler that itself caused lots of allocations

--- lib/new_relic/agent/samplers/class_count_sampler.rb
+++ lib/new_relic/agent/samplers/class_count_sampler.rb
@@ -6,21 +6,10 @@ module NewRelic
       named :class_count
 
       def self.supported_on_this_platform?
-        ObjectSpace.respond_to?(:each_object)
+        ObjectSpace.respond_to?(:count_objects)
       end
 
       def poll
-        class_counts = Hash.new(0)
-        ObjectSpace.each_object do |obj|
-          class_name = obj.class.to_s rescue '<unknown>'
-          if class_name.start_with?('NewRelic')
-            class_counts[class_name] += 1
-          end
-        end
-
-        event = class_counts.merge(:pid => Process.pid)
-        Agent.record_custom_event(:NewRelicClassCounts, event)
-
         event = ObjectSpace.count_objects.merge(:pid => Process.pid)
         Agent.record_custom_event(:RubyObjectTypeCounts, event)
       end
